c48025050cede2c7cb45f0476c6280a019eb0385
양원준 <97458548+ywj9811@users.noreply.github.com>
2022-12-09 02:01:06 +0900

    fix: 낫싱 추가

--- src/main/java/baseball/domain/OutputString.java
+++ src/main/java/baseball/domain/OutputString.java
@@ -3,6 +3,7 @@ package baseball.domain;
 public enum OutputString {
     BALL("볼"),
     STRIKE("스트라이크"),
+    NOTHING("낫싱"),
     START("숫자 야구 게임을 시작합니다."),
     INPUT("숫자를 입력해주세요 : "),
     FINAL("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
