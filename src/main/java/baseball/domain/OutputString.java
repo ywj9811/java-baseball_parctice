ec47508e321dd814ad33cc1f9e709be685631a8a
양원준 <97458548+ywj9811@users.noreply.github.com>
2022-12-09 01:34:15 +0900

    feat: 출력을 위한 enum 생성

---
+++ src/main/java/baseball/domain/OutputString.java
@@ -0,0 +1,21 @@
+package baseball.domain;
+
+public enum OutputString {
+    BALL("볼"),
+    STRIKE("스트라이크"),
+    START("숫자 야구 게임을 시작합니다."),
+    INPUT("숫자를 입력해주세요 : "),
+    FINAL("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
+    RESTART("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+
+    private String print;
+
+    OutputString(String print) {
+        this.print = print;
+    }
+
+    public String getPrint() {
+        return print;
+    }
+}
+
