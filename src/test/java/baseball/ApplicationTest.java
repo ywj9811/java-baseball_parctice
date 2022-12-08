cd768edd7d40e3638d5c239d1ab9b4399a5fd828
양원준 <97458548+ywj9811@users.noreply.github.com>
2022-12-09 04:00:19 +0900

    test: 테스트 케이스 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -21,9 +21,73 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 재시작X() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("246", "135", "2");
+                    assertThat(output()).contains("낫싱", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5
+        );
+    }
+
+    @Test
+    void 예외_테스트5() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> runException("246", "135", "1", "222"))
+                            .isInstanceOf(IllegalArgumentException.class);
+                },
+                1, 3, 5, 2, 4, 6
+        );
+    }
+    @Test
+    void 예외_테스트6() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> runException("246", "135", "4"))
+                            .isInstanceOf(IllegalArgumentException.class);
+                },
+                1, 3, 5, 2, 4, 6
+        );
+    }
+
+    @Test
+    void 예외_테스트7() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> runException("246", "135", "a"))
+                            .isInstanceOf(IllegalArgumentException.class);
+                },
+                1, 3, 5, 2, 4, 6
+        );
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void 예외_테스트2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("123a"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void 예외_테스트3() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("112"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void 예외_테스트4() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("asd"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
