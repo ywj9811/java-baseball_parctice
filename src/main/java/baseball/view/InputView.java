a2241a9e5ea553e4b4cddfab8499af1a441e0e0d
양원준 <97458548+ywj9811@users.noreply.github.com>
2022-12-09 02:38:44 +0900

    refactor: import 삭제

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,8 +3,6 @@ package baseball.view;
 import baseball.domain.OutputString;
 
 public class InputView {
-    private OutputString outputString;
-
     public String inputNumber() {
         String input = camp.nextstep.edu.missionutils.Console.readLine();
         System.out.println();
