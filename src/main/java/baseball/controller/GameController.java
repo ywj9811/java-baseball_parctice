e5e7fc395cd3fbd73f0cf16adfffa1d67b3f6a18
양원준 <97458548+ywj9811@users.noreply.github.com>
2022-12-09 04:13:42 +0900

    refactor: GameController 메소드 분리

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -17,22 +17,22 @@ public class GameController {
     private OutputView outputView = new OutputView();
     private List<Integer> randomNumber = new ArrayList<>();
     private String inputNum;
-    private int ballCount = 0;
-    private int strikeCount = 0;
+    private int ballCount;
+    private int strikeCount;
+
+    public void resetCount() {
+        ballCount = 0;
+        strikeCount = 0;
+    }
 
     public void gameProcess(int idx) {
         ballCount += checkNumber.checkBall(randomNumber, inputNum, idx);
         strikeCount += checkNumber.checkStrike(randomNumber, inputNum, idx);
     }
 
-    public void gameStartAndEnd() {
-        ballCount = 0;
-        strikeCount = 0;
-        outputView.startGame();
-        randomNumber = computerNumber.makeComputerNumber();
+    public void repeatProgress() {
         while (strikeCount != 3) {
-            ballCount = 0;
-            strikeCount = 0;
+            resetCount();
             outputView.printInputNumber();
             inputNum = inputView.inputNumber();
             validation.playerInputNumberValidation(inputNum);
@@ -41,12 +41,23 @@ public class GameController {
             }
             outputView.printResult(strikeCount, ballCount);
         }
-        outputView.printEnd();
-        outputView.printRestart();
+    }
+
+    public void choiceRestart() {
         String restart = inputView.inputNumber();
         validation.playerRestartValidation(restart);
         if (restart.equals("1")) {
             gameStartAndEnd();
         }
+    }
+
+    public void gameStartAndEnd() {
+        resetCount();
+        outputView.startGame();
+        randomNumber = computerNumber.makeComputerNumber();
+        repeatProgress();
+        outputView.printEnd();
+        outputView.printRestart();
+        choiceRestart();
     }
 }
