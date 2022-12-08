c20cd65cd1e86f46ee14b35437afe3a8f2f61396
양원준 <97458548+ywj9811@users.noreply.github.com>
2022-12-09 04:48:03 +0900

    refactor: 상수 생성 및 상수 사용으로 변경

--- src/main/java/baseball/service/ComputerNumber.java
+++ src/main/java/baseball/service/ComputerNumber.java
@@ -6,11 +6,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ComputerNumber {
+    private static final int MAX_NUM = 9;
+    private static final int MIN_NUM = 1;
+    private static final int SIZE = 3;
+
     public List<Integer> makeComputerNumber() {
         List<Integer> computerNumber = new ArrayList<>();
 
-        while (computerNumber.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+        while (computerNumber.size() < SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_NUM, MAX_NUM);
             if (!computerNumber.contains(randomNumber)) {
                 computerNumber.add(randomNumber);
             }
